2130b791e4cd5f7d762bef88f6d46ab832bea689
Hank Tom <hanktom@gmail.com>
2020-12-11 21:25:02 +0800

    BMI功能,Log,Toast,AlertDialog

--- app/src/main/java/com/tom/bmi/MainActivity.kt
+++ app/src/main/java/com/tom/bmi/MainActivity.kt
@@ -2,10 +2,29 @@ package com.tom.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun bmi(view: View) {
+        val weight = ed_weight.text.toString().toFloat()
+        val height = ed_height.text.toString().toFloat()
+        val bmi = weight / (height * height)
+        Log.d("BMI", bmi.toString())
+        Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setMessage(bmi.toString())
+            .setTitle("Your BMI")
+            .setPositiveButton("OK", null)
+            .setNeutralButton("Cancel", null)
+            .show()
     }
 }
